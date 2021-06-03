682d0568d83c33f5e41d033e3d997feb86b7cd9d
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2021-06-03 22:22:48 +0530

    refactor: upgrade docus & directory structure

---
+++ docus.config.ts
@@ -0,0 +1,21 @@
+export default {
+  title: 'UNICEF :: Documentation',
+  url: 'https://geospoc.github.io/unc-sch-documentation',
+  theme: {
+    colors: {
+      primary: '#2F4F4F'
+    },
+    header: {
+      title: false,
+      logo: {
+        light: '/icon.png',
+        dark: '/icon.png'
+      }
+    }
+  },
+  twitter: 'geospoc',
+  github: {
+    repo: 'geospoc/unc-sch-documentation',
+    branch: 'main'
+  }
+}
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,39 @@
-import { withDocus } from 'docus'
+import { withDocus } from 'docus';
 
-// Learn more at https://docus.dev
 export default withDocus({
-  // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
-  ssr: false,
-  // Target (https://go.nuxtjs.dev/config-target)
-  target: 'static',
-  // Docus settings
-  docus: {
-    primaryColor: '#2F4F4F'
-  },
-  // Router settings for deployment
-  router: {
-    base: '/unc-sch-documentation/',
+  rootDir: __dirname,
+  head: {
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'UNICEF/GeoSpoc' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'twitter:site', name: 'twitter:site', content: '@geospoc' },
+      {
+        hid: 'twitter:card',
+        name: 'twitter:card',
+        content: 'summary_large_image'
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'https://image.nuxtjs.org/social.png'
+      },
+      {
+        hid: 'og:image:secure_url',
+        property: 'og:image:secure_url',
+        content: 'https://image.nuxtjs.org/social.png'
+      },
+      {
+        hid: 'og:image:alt',
+        property: 'og:image:alt',
+        content: 'UNICEF/GeoSpoc'
+      },
+      {
+        hid: 'twitter:image',
+        name: 'twitter:image',
+        content: 'https://image.nuxtjs.org/social.png'
+      }
+    ]
   },
 })
