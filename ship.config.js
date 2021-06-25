947d6b710c1eb815e736b15126793140785b84f7
Vinayak Kulkarni <vinayak@geospoc.com>
2021-06-26 02:14:33 +0530

    fix: ignore the CHANGELOG markdown

--- ship.config.js
+++ ship.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   installCommand: () => 'npm ci --prefer-offline --no-audit --no-optional',
-  beforeCommitChanges: () => 'npm run lintfix',
   buildCommand: () => null,
   publishCommand: ({ tag }) => `echo "Releasing ${tag} version of UNICEF docs"`,
 };
