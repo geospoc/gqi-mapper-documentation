c33b8bc6d1e82a1e9b92fdf21087c28c38b8ca89
Vinayak Kulkarni <vinayak@geospoc.com>
2021-06-26 02:10:07 +0530

    chore: update beforeCommit

--- ship.config.js
+++ ship.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   installCommand: () => 'npm ci --prefer-offline --no-audit --no-optional',
+  beforeCommitChanges: () => 'npm run lintfix',
   buildCommand: () => null,
   publishCommand: ({ tag }) => `echo "Releasing ${tag} version of UNICEF docs"`,
 };
