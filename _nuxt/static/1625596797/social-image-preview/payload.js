79789b1d1819553cb4a35cf7d9686f0c5fdec34f
vinayakkulkarni <vinayakkulkarni@users.noreply.github.com>
2021-07-08 12:43:48 +0000

    deploy: 4ebfb5973497fdf150f79bb2cdf2aff80cc9275e

--- _nuxt/static/1625596797/social-image-preview/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/social-image-preview", {data:[{_img:{}}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1625748152/social-image-preview/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/social-image-preview", {data:[{_img:{"/unc-sch-documentation/_ipx/unc-sch-documentation/icon.png":"\u002Func-sch-documentation\u002F_nuxt\u002Fimage\u002F46b1af.png"}}],fetch:{},mutations:void 0});
