66c5e0ff4a0be32f61b564871e671085938ead3f
tusharkgeospoc <tushar.kshirsagar@geospoc.com>
2021-08-09 18:50:55 +0530

    fix: apckage-lock file

--- ship.config.js
+++ ship.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  installCommand: () => 'npm ci --prefer-offline --no-audit --no-optional',
+  installCommand: () => 'npm i --prefer-offline --no-audit --no-optional',
   buildCommand: () => null,
   publishCommand: ({ tag }) => `echo "Releasing ${tag} version of UNICEF docs"`,
 };
