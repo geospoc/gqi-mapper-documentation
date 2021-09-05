ffb1857050d4972a69000c117e947e596494cfe8
vinayakkulkarni <vinayakkulkarni@users.noreply.github.com>
2021-09-05 19:44:10 +0000

    deploy: 285514946e0d5424cab41905ac85de5ae85593f4

--- _nuxt/static/1630649671/social-image-preview/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/social-image-preview", {data:[{_img:{"/gqi-mapper-documentation/_ipx/gqi-mapper-documentation/icon.png":"\u002Fgqi-mapper-documentation\u002F_nuxt\u002Fimage\u002F0905d2.png"}}],fetch:{},mutations:void 0});
